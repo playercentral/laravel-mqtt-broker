7bedfd5c73f7567e1dc9ed4c1681efb37d7e7a66
Karoly Nemethy <olythy@gmail.com>
2026-04-21 16:47:20 +0200

    fix: correct servize provider registration

--- src/MqttServiceProvider.php
+++ src/MqttServiceProvider.php
@@ -10,28 +10,36 @@ use PlayerCentral\MqttBroker\Broadcasters\MqttBroadcaster;
 use PlayerCentral\MqttBroker\Console\Commands\InstallMqttCommand;
 use PlayerCentral\MqttBroker\Contracts\MqttClientFactoryInterface;
 use PlayerCentral\MqttBroker\Mqtt\PhpMqttClientFactory;
+use Illuminate\Support\Facades\Broadcast;
 
 class MqttServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
         $this->publishes([
-            __DIR__.'/config/mqtt.php' => config_path('mqtt.php'),
+            __DIR__ . '/config/mqtt.php' => config_path('mqtt.php'),
         ], 'mqtt-config');
 
-        $this->app->resolving(BroadcastManager::class, function (BroadcastManager $manager): void {
-            $manager->extend('mqtt', function ($app, array $config): MqttBroadcaster {
-                return new MqttBroadcaster(
-                    $config,
-                    $app->make(MqttClientFactoryInterface::class)
-                );
-            });
+        Broadcast::extend('mqtt', function ($app, array $config) {
+            return new MqttBroadcaster(
+                $config,
+                $app->make(MqttClientFactoryInterface::class)
+            );
         });
+
+        // $this->app->resolving(BroadcastManager::class, function (BroadcastManager $manager): void {
+        //     $manager->extend('mqtt', function ($app, array $config): MqttBroadcaster {
+        //         return new MqttBroadcaster(
+        //             $config,
+        //             $app->make(MqttClientFactoryInterface::class)
+        //         );
+        //     });
+        // });
     }
 
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/config/mqtt.php', 'mqtt');
+        $this->mergeConfigFrom(__DIR__ . '/config/mqtt.php', 'mqtt');
         $this->app->singleton(MqttClientFactoryInterface::class, PhpMqttClientFactory::class);
 
         if ($this->app->runningInConsole()) {
